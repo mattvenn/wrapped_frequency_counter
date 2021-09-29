9a5bd999fb4ff71f82a45f7d75b81c3252672248
Matt Venn <matt@mattvenn.net>
2021-09-29 12:16:05 +0200

    power line config and cocotb makefile

--- config.tcl
+++ config.tcl
@@ -31,8 +31,8 @@ set ::env(GLB_RT_MAXLAYER) 5
 
 
 
-set ::env(VDD_NETS) [list {vccd1} {vccd2} {vdda1} {vdda2}]
-set ::env(GND_NETS) [list {vssd1} {vssd2} {vssa1} {vssa2}]
+set ::env(VDD_NETS) [list {vccd1}]
+set ::env(GND_NETS) [list {vssd1}]
 
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
