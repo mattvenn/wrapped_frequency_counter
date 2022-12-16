7d294428b66e61cb4ac005ae1abca10ca3ea86ec
Matt Venn <matt@mattvenn.net>
2022-12-16 11:05:14 +0100

    updates for cocotb 1.7.2 and extend count period to make test pass with change to vexrisc

--- caravel_frequency_counter/test_frequency_counter.py
+++ caravel_frequency_counter/test_frequency_counter.py
@@ -7,36 +7,36 @@ from test_seven_segment import read_segments
 @cocotb.test()
 async def test_start(dut):
     clock = Clock(dut.clk, 25, units="ns") # 40M
-    cocotb.fork(clock.start())
+    cocotb.start_soon(clock.start())
     
-    dut.RSTB <= 0
-    dut.power1 <= 0;
-    dut.power2 <= 0;
-    dut.power3 <= 0;
-    dut.power4 <= 0;
+    dut.RSTB.value = 0
+    dut.power1.value = 0;
+    dut.power2.value = 0;
+    dut.power3.value = 0;
+    dut.power4.value = 0;
 
     await ClockCycles(dut.clk, 8)
-    dut.power1 <= 1;
+    dut.power1.value = 1;
     await ClockCycles(dut.clk, 8)
-    dut.power2 <= 1;
+    dut.power2.value = 1;
     await ClockCycles(dut.clk, 8)
-    dut.power3 <= 1;
+    dut.power3.value = 1;
     await ClockCycles(dut.clk, 8)
-    dut.power4 <= 1;
+    dut.power4.value = 1;
 
     await ClockCycles(dut.clk, 80)
-    dut.RSTB <= 1
+    dut.RSTB.value = 1
 
     # start the input signal
     period_us = 4 # 25kHz
     # default update period is 1200 cycles
-    input_signal = cocotb.fork(Clock(dut.signal, period_us,  units="us").start())
+    input_signal = cocotb.start_soon(Clock(dut.signal, period_us,  units="us").start())
 
     # wait for the project to become active - time out if necessary - should happen around 165us
     await with_timeout(RisingEdge(dut.uut.mprj.wrapped_frequency_counter_2.active), 500, 'us')
 
     # let counter settle 
-    await ClockCycles(dut.clk, 6000)
+    await ClockCycles(dut.clk, 7000)
 
     # read the 7 segment display
     reading = await read_segments(dut)
